1741532748aaf2d02b57d27785e23468aaf4fa67
Sean Francis N. Ballais <sean@seanballais.com>
2019-05-03 15:23:30 +0800

    Shortened the number of parameters for the GA.

--- generate_parameters.py
+++ generate_parameters.py
@@ -7,10 +7,10 @@ def generate_parameters(use_ga=0):
     num_labels = 26
 
     lambda_values = [ 0.0001, 0.001, 0.01, 0.1, 1.0, 10 ]
-    population_sizes = [ 5, 10, 15 ] if use_ga else [ 50 ]
-    num_random_parents = [ 0, 1, 2, 3, 4 ] if use_ga else [ 0 ]
-    num_generations = [ 10, 10000, 15000 ] if use_ga else [ 3 ]
-    mutation_probabilities = [ 0.07, 0.1 ] if use_ga else [ 0 ]
+    population_sizes = [ 10 ] if use_ga else [ 50 ]
+    num_random_parents = [ 0, 1 ] if use_ga else [ 0 ]
+    num_generations = [ 10, 15000 ] if use_ga else [ 3 ]
+    mutation_probabilities = [ 0.1 ] if use_ga else [ 0 ]
     training_items = 16000
     testing_items = 4000
 
